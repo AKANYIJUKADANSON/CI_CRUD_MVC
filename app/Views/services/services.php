1100ad49d94116f7f80cdadaaae66250ffdfe45a
DANSON.DEV <dantechx02@gmail.com>
2025-02-14 17:02:26 +0300

    Created the services view

---
+++ app/Views/services/services.php
@@ -0,0 +1,8 @@
+
+    <h1 style="color: orangered; font-size: 40px; text-align: center">Our Services</h1>
+    <ul>
+        <li>Web Development</li>
+        <li>Mobile Development</li>
+        <li>Desktop Development</li>
+        <li>Cloud Development</li>
+    </ul>
