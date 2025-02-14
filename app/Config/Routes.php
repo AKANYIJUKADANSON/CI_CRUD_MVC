9f03ff21a69c1c854a7fdd28b3d71127886daeb6
DANSON.DEV <dantechx02@gmail.com>
2025-02-14 16:51:07 +0300

    Added routes for the pages created

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,13 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Pages;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Adding the route for the pages
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'index']);
